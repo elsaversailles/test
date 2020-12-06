471ecd97867d5ebd5170d1711034e4e376a93d74
Elsa Versailles <50725388+elsaversailles@users.noreply.github.com>
2020-12-06 19:20:45 +0800

    Create Assignment.cpp

---
+++ Assignment.cpp
@@ -0,0 +1,61 @@
+#include<iostream
+using namespace std
+
+int main()
+{
+    string name, course, year, address;
+    cout<<"Enter your name:";
+    cin>>name; 
+    cout<<"Enter your course:";
+    cin>>course; 
+    cout<<"Enter your year:";
+    cin>>year; 
+    cout<<"Enter your address:";
+    cin>>address;  
+    return 0;
+}
+
+{
+    int a,b,sum,diff,mul,div,rem;
+	printf("Enter two number \n");
+	scanf("%d%d",&a,&b);
+	sum=a+
+	mul=a*b;
+	div=a/b;
+	rem=a%b;
+	printf("Addition is %d\n",sum);
+	printf("Subtraction is %d\n",diff);
+	printf("Multiplication is %d\n",mul);
+	printf("Division is %d\n",div);
+	printf("Remainder is %d",rem);
+	return 0;
+}
+
+{
+int a, b;
+a=6, b=16;
+if (a>b)
+{
+printf("%d is greater than %d\n", a, b);
+}
+if (a<b)
+{
+printf("%d is less than %d\n", a, b);
+}
+if (a>=b)
+{
+printf("%d is greater than or equal to %d\n", a, b);
+}
+if (a<=b)
+{
+printf("%d is less than or equal to %d\n", a, b);
+}
+if (a==b)
+{
+printf("%d is equal to %d\n", a, b);
+}
+if (a!=b)
+{
+printf("%d is not equal to %d\n", a, b);
+}
+}
